3b7526ce1cfeca122d713b93058d4e4fcbe1e04b
Mohammed Al-Khalifah <mohak2009@hotmail.com>
2020-06-05 16:49:53 +0300

    clean up

--- server.js
+++ server.js
@@ -24,12 +24,12 @@ const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
-
 app.get('/', (req, res)=> { res.send(db.users) })
 app.post('/signin', (req, res)=>{signin.handleSignin(req, res, db, bcrypt)})
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res)=>{profile.handleProfileGet(req, res, db)})
 app.post('/image', (req, res)=>{image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res)=>{image.handleApiCall(req, res)})
 
 app.listen(3000, ()=> {
     console.log('app is running on port 3000');
