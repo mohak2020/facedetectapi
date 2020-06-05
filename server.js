0f8f21e4c5187255ae0f8a9239a89e3455d4d380
Mohammed Al-Khalifah <mohak2009@hotmail.com>
2020-06-05 18:45:34 +0300

    correcting port number

--- server.js
+++ server.js
@@ -24,15 +24,15 @@ const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/', (req, res)=> { res.send(db.users) })
+app.get('/', (req, res)=> { res.send('its working') })
 app.post('/signin', (req, res)=>{signin.handleSignin(req, res, db, bcrypt)})
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res)=>{profile.handleProfileGet(req, res, db)})
 app.post('/image', (req, res)=>{image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res)=>{image.handleApiCall(req, res)})
 
-app.listen(3000, ()=> {
-    console.log('app is running on port 3000');
+app.listen(process.env.PORT||3000, ()=> {
+    console.log(`app is running on port 3000${process.env.PORT}`);
   })
 
 
